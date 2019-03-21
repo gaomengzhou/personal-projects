ac7d8bb7d55b336a2979d90d535f6df2bba97d49
gaomengzhou <gao@qq.com>
2019-03-21 14:39:04 +0800

    更新改错

--- php/config.php
+++ php/config.php
@@ -1,6 +1,11 @@
 <?php
+// @header("content-type:text/html;charset=utf8");
+// mysql_connect("localhost:3306","root","root");
+// mysql_select_db("project_for_go_fishing");
+// mysql_query("set character set 'utf8'");
+
 @header("content-type:text/html;charset=utf8");
-mysql_connect("localhost:3306","root","root");
-mysql_select_db("project_for_go_fishing");
+mysql_connect("b-5je3g8kdfewgyg.bch.rds.gz.baidubce.com:3306","b_5je3g8kdfewgyg","HNHSEsD8tWmmbmaU");
+mysql_select_db("b_5je3g8kdfewgyg");
 mysql_query("set character set 'utf8'");
 ?>
