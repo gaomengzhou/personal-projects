660c3fa03d88b5a62f969560234b6a0175d2a37c
gaomengzhou <gao@qq.com>
2019-03-21 21:00:10 +0800

    更新

--- php/config.php
+++ php/config.php
@@ -5,7 +5,7 @@ mysql_select_db("project_for_go_fishing");
 mysql_query("set character set 'utf8'");
 
 // @header("content-type:text/html;charset=utf8");
-// mysql_connect("b-5je3g8kdfewgyg.bch.rds.gz.baidubce.com:3306","b_5je3g8kdfewgyg","HNHSEsD8tWmmbmaU");
+// mysql_connect("b-5je3g8kdfewgyg.bch.rds.gz.baidubce.com:3306","b_5je3g8kdfewgyg","19891024");
 // mysql_select_db("b_5je3g8kdfewgyg");
 // mysql_query("set character set 'utf8'");
 ?>
