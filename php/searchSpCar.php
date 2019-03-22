374a72cc34c6a3b76e135a7429e5e2e0a2176815
gaomengzhou <gao@qq.com>
2019-03-22 10:29:52 +0800

    更新

--- php/searchSpCar.php
+++ php/searchSpCar.php
@@ -1,6 +1,8 @@
 <?php
 @require_once("config.php");
-$str = "SELECT count(*) FROM `shoppingcar`";
+$userid = $_GET["userid"];
+
+$str = "SELECT count(*) FROM `shoppingcar` WHERE userid =$userid AND type=1";
 $result = mysql_query($str);
 $item  = mysql_fetch_array($result);
 $CarNum=$item[0];
